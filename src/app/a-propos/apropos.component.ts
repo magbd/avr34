73b35892fa93c7b9e2cf532dae54fbd6d0b86732
Magali <mag.baude@hotmail.fr>
2017-09-02 18:36:00 +0200

    création module à propos (page à créer)

---
+++ src/app/a-propos/apropos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apropos',
+  template: `
+  <h1>Page à propos à créer</h1>
+  `
+})
+
+export class AproposComponent {}
+
---
+++ src/app/a-propos/apropos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { AproposComponent } from './apropos.component'
+
+@NgModule({
+    imports: [ CommonModule  ],
+    declarations: [ AproposComponent ],
+    exports: [ AproposComponent ],
+    providers: []
+})
+export class AproposModule {}
