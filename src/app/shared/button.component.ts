56b81b2e9535d65a30c22e2e527abeaf813b773a
Magali <mag.baude@hotmail.fr>
2017-09-02 18:35:40 +0200

    création module shared et test button générique

---
+++ src/app/shared/button.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'button-component',
+  template: `<button>UN BOUTON</button>`
+})
+export class ButtonComponent{}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { ButtonComponent } from './button.component'
+
+@NgModule({
+    imports: [ CommonModule ],
+    declarations: [ ButtonComponent ],
+    exports: [ ButtonComponent ],
+    providers: []
+})
+export class SharedModule {}
